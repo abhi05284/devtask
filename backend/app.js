46eff11d848de080b41d5715eded75d37a562887
abhi05284 <shivamuser0@gmail.com>
2024-12-22 15:01:36 +0530

    Initial commit with Devtask project

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+
+app.use(express.json());
+app.use(cors());
+
+const mongoUrl = process.env.MONGODB_URL;
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit the app if the connection fails
+  }
+};
+
+connectDB();
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/profile", profileRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
+});
